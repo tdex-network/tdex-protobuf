a9cfbc5a2588bde05a429152f9efbee415b82416
Marco Argentieri <marco@vulpem.com>
2020-05-25 12:57:40 +0200

    Use grpc_js when compiling grpc stubs (#9)

--- js/operator_grpc_pb.js
+++ js/operator_grpc_pb.js
@@ -1,7 +1,7 @@
 // GENERATED CODE -- DO NOT EDIT!
 
 'use strict';
-var grpc = require('grpc');
+var grpc = require('@grpc/grpc-js');
 var operator_pb = require('./operator_pb.js');
 
 function serialize_DepositAddressReply(arg) {
